5b18417108fae1a9274090685cb512fa6b0da425
mfberkdemir <mfberkdemir@gmail.com>
2019-08-31 21:26:48 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hangfire Prometheus Exporter
+
+
+
+
+
+
+
+
+
+
---
+++ hangfireExporter/Controllers/metricsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hangfireExporter.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class metricsController : ControllerBase
+    {        
+        public string Get()
+        {
+            return "";
+        }        
+    }
+}
